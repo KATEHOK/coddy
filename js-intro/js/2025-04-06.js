93cf71e954b6a8d639b6baeab2d12273a3047b0e
Katehok <a.timin2003@mail.ru>
2025-04-13 15:40:33 +0300

    After cw 2025-04-13

--- js-intro/js/2025-04-06.js
+++ js-intro/js/2025-04-06.js
@@ -1,27 +1,36 @@
 // + - * /
-let num1, num2, sign, result = NaN;
 
-num1 = Number(prompt("Введите первое число", "0"));
-num2 = Number(prompt("Введите второе число", "0"));
-sign = prompt("Введите действие (+, -, *, /)", "+");
-
-switch (sign) {
-    case "+":
-        result = num1 + num2;
-        break;
-    case "-":
-        result = num1 - num2;
-        break;
-    case "*":
-        result = num1 * num2;
-        break;
-    case "/":
-        result = num1 / num2;
-        break;
+function calculate(sign, num1, num2) {
+    let result = NaN;
+    switch (sign) {
+        case "+":
+            result = num1 + num2;
+            break;
+        case "-":
+            result = num1 - num2;
+            break;
+        case "*":
+            result = num1 * num2;
+            break;
+        case "/":
+            if (0 != num2) result = num1 / num2;
+            break;
+    }
+    return result;
 }
 
-if (isNaN(result)) {
-    alert("Некорректный ввод!");
-} else {
-    alert(`Ответ: ${result}`);
+function calculator() {
+    if (!confirm("Запустить калькулятор?")) return false;
+
+    let num1 = Number(prompt("Введите первое число", "0"));
+    let num2 = Number(prompt("Введите второе число", "0"));
+    let sign = prompt("Введите действие (+, -, *, /)", "+");
+    let result = calculate(sign, num1, num2);
+
+    if (isNaN(result)) alert("Некорректный ввод!");
+    else alert(`Ответ: ${result}`);
+    return true;
 }
+
+let needRun = true;
+while (needRun) needRun = calculator();
